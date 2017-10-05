6378ce43b6df2356dce8c58cfd59938f09034d09
yujie <seven8355@gmail.com>
2017-10-05 14:19:45 +0800

    chore(app): control panel

--- app/app.js
+++ app/app.js
@@ -1,2 +1,25 @@
+'use strict'
+
+// web framework
+
+import 'imports-loader?define=>false,exports=>false,this=>window!mustache/mustache'
+
+import Plotly from 'plotly.js/dist/plotly.min.js'
+
+// custom modules
+
 import './app.sass'
 import './index.pug'
+
+// load resource data
+
+import res from './res.json'
+
+///////////////////////////////////////////////////////
+
+// control panel
+
+document.querySelector('#setting').innerHTML = Mustache.render(
+  document.querySelector('#setting script').innerHTML,
+  { id: Object.keys(res) }
+)
