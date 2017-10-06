b1be99f18a0f070d833580da4ecf32fcd5aba0d1
yujie <seven8355@gmail.com>
2017-10-06 09:53:51 +0800

    chore(app): basic plots

--- app/app.js
+++ app/app.js
@@ -17,9 +17,45 @@ import res from './res.json'
 
 ///////////////////////////////////////////////////////
 
+const app = {
+  result_tmpl: document.querySelector('#results script').innerHTML,
+  show_tids: {}
+}
+Mustache.parse(app.result_tmpl)
+
+const plotResult = () => {
+  let tids = Object.keys(app.show_tids).filter(tid => app.show_tids[tid])
+
+  // plot results
+
+  document.querySelector('#results').innerHTML = Mustache.render(app.result_tmpl, { tid: tids })
+
+  for (let tid of tids) {
+    Plotly.newPlot(
+      `plot_${tid}`,
+      [{
+        fill: 'tozeroy',
+        mode: 'none',
+        type: 'scatter',
+        x: res[tid].plot.x,
+        y: res[tid].plot.y
+      }],
+      {
+        title: `[${res[tid].sequence_info.gene_name}] (${tid})`
+      }
+    )
+  }
+}
+
 // control panel
 
 document.querySelector('#setting').innerHTML = Mustache.render(
   document.querySelector('#setting script').innerHTML,
-  { id: Object.keys(res) }
+  { tid: Object.keys(res) }
 )
+
+Array.from(document.querySelectorAll('#setting label'), dom => dom.onclick = () => {
+  app.show_tids[dom.dataset.tid] = app.show_tids[dom.dataset.tid] ? app.show_tids[dom.dataset.tid] ^ 1 : 1
+
+  plotResult()
+})
