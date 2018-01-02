1fe7a1934e8e93beabfd763886a79942508067be
yujie <seven8355@gmail.com>
2018-01-02 15:54:06 +0800

    chore(app): basic layout

--- app/app.js
+++ app/app.js
@@ -1,12 +1,30 @@
+'use strict'
+
+// web framework
+
+import 'imports-loader?define=>false,exports=>false,this=>window!mustache/mustache'
+import 'semantic-ui/dist/semantic.min.css'
+
 const socket = io()
+
+// custom modules
 
 import './app.sass'
 import './index.pug'
+import db from './res/dataset.json'
 
-if ('development' === process.env.NODE_ENV) {
+// for hot module replacement in development
+
+if ('development' === process.env.NODE_ENV)
   require('webpack-hot-middleware/client').subscribe(event => {
     if ('hmr' === event.action) window.location.reload()
   })
-}
 
-socket.on('news', it => console.log(it))
+///////////////////////////////////////////////////////////////////////////////////////
+
+const app = {
+  search_tmpl: document.querySelector('#search script').innerHTML
+}
+Mustache.parse(app.search_tmpl)
+
+document.querySelector('#search tbody').innerHTML = Mustache.render(app.search_tmpl, { db: db })
